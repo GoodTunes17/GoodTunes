71d07420cfb83a9b478d636e976599cba7324115
ppreziotti <patpreziotti@gmail.com>
2017-07-16 21:06:52 -0400

    merged conflicts

--- app/components/children/Scrape.js
+++ app/components/children/Scrape.js
@@ -6,26 +6,50 @@ var Scrape = React.createClass({
 
   // Here we set a generic state associated with the text being searched for
   getInitialState: function () {
-    return { 
+    return {
 
     };
   },
 
   // This function will respond to the user input
   handleChange: function (event) {
-    
+
   },
 
   // When a user submits...
   handleSubmit: function (event) {
-    
+
   },
-  // Here we describe this component's render method
+
+    handleClick1: function (result, e) {
+    console.log("Clicked! " +
+      result._id + "  " +
+      result.title
+    );
+    this.props.savedArticles(result);
+  },
+  
+  // HERE we render the scraped info -  then send it to main.js
+  
   render: function () {
     return (
+
       <div>
+        {this.props.scrape.map(function (search, i) {
+ var boundClick1 = this.handleClick1.bind(this, search);
+          return (
+            <div>
+              <p> ARTIST: {search.artist} - SONG: {search.title}</p>
+ <button key={i} onClick={boundClick1}> save </button>
+              <p> _________________________________</p>
+            </div>
+
+          );
+        }.bind(this)
+        )
+        }
       </div>
-    );
+    )
   }
 });
 
