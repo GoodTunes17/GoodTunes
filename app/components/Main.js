9e86fcea6b02d6ab211d4cfe2a9962c4580c7208
ppreziotti <patpreziotti@gmail.com>
2017-08-01 21:29:42 -0400

    updated userLogout to reset state for playlist

--- app/components/Main.js
+++ app/components/Main.js
@@ -130,27 +130,6 @@ var Main = React.createClass({
     console.log("playlist = " + this.state.playlist[0]);
   },
 
-
-  // this will change the "saved" database property to true
-
-  // savedArticles: function (result) {
-  //   console.log("This will need to be saved: " + result.artist + "whose id is: " + result._id)
-  //   helpers.postArticle(result._id).then(() => {
-  //     this.getAllArticles()
-  //   })
-  // },
-
-  // this will change the "saved" database property to false
-
-  // deletedArticle: function (result) {
-  //   console.log("delete!");
-  //   console.log("This will need to be un-saved: " + result.artist + "whose id is: " + result._id)
-  //   helpers.deleteArticle(result._id);
-  //   this.getAllArticles();
-  //   // shouldn't this refresh the saved articles? 
-  // },
-
-
   rating: function (result) {
     var songId = result[0];
     var rating = result[1];
@@ -223,17 +202,20 @@ var Main = React.createClass({
       }
       // If the login is successful, render the playlist component with the welcome message
       else {
-        this.setState({ email: result.email });
-        this.setState({ isLoggedIn: true });
+        this.setState({ 
+          email: result.email,
+          isLoggedIn: true
+        });
+        this.playlist2();
         this.props.history.push('/Playlist');
       }
     }.bind(this));
   },
 
-  // playlist: function () {
-  //   console.log("sending here - " + this.state.email)
-  //   return axios.post("/playlist/" + this.state.email)
-  // },
+  playlist: function () {
+    console.log("sending here - " + this.state.email)
+    return axios.post("/playlist/" + this.state.email)
+  },
 
   userSignup: function (result) {
     helpers.createUser(result.email, result.password).then(function(response) {
@@ -253,8 +235,11 @@ var Main = React.createClass({
   },
 
   userLogout: function () {
-    this.setState({ email: "" });
-    this.setState({ isLoggedIn: false });
+    this.setState({ 
+      email: "",
+      isLoggedIn: false,
+      playlist: []
+    });
     helpers.logout().then(function (response) {
       console.log(response);
     }.bind(this));
@@ -325,7 +310,7 @@ var Main = React.createClass({
     var url = "https://open.spotify.com/embed?uri=spotify:track:" + this.state.id;
 
 
-    var children = React.Children.map(this.props.children, function (child) { return React.cloneElement(child, { scrapedArticles: this.state.scrapedArticles, savedArticles: this.savedArticles, playSong: this.playSong, deletedArticle: this.deletedArticle, id: this.state.id, playlist2: this.state.playlist, rating: this.rating, userLogin: this.userLogin, userSignup: this.userSignup, userLogout: this.userLogout, isLoggedIn: this.state.isLoggedIn, email: this.state.email, message: this.state.message }) }.bind(this))
+    var children = React.Children.map(this.props.children, function (child) { return React.cloneElement(child, { scrapedArticles: this.state.scrapedArticles, savedArticles: this.savedArticles, playSong: this.playSong, deletedArticle: this.deletedArticle, id: this.state.id, playlist: this.state.playlist, rating: this.rating, userLogin: this.userLogin, userSignup: this.userSignup, userLogout: this.userLogout, isLoggedIn: this.state.isLoggedIn, email: this.state.email, message: this.state.message }) }.bind(this))
 
     if (this.state.isLoggedIn === true) {
       var welcomeStatement = "Welcome, " + this.state.email + "!";
